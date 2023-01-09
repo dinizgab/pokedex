69875ed33bc3484c368601851e9e6d03de2102db
Gabriel <gabrielpacirnediniz@gmail.com>
2023-01-09 10:59:52 -0300

    Creating the pokemon cards

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,3 +1,11 @@
-export default function PokemonCard() {
+interface PokemonCardProps {
+    name: string,
+}
 
+export default function PokemonCard(props: PokemonCardProps) {
+    return (
+        <div className="bg-[#FFEAEE] w-80 h-80">
+            <h1>{props.name}</h1>
+        </div>
+    )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
+import PokemonCard from "../components/PokemonCard";
+
+interface PokemonInterface {
+  name: string;
+  url: string;
+}
 
 export default function Home() {
+  const [pokemons, setPokemons] = useState<Array<PokemonInterface>>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => setPokemons(res.data.results));
+  }, []);
+
   return (
     <>
       <NavBar />
-      <div>aaaaaaa</div>
+      <div className="grid grid-cols-4 gap-x-4 gap-y-4 px-8">
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.name} name={pokemon.name} />
+        ))}
+      </div>
     </>
   );
 }
