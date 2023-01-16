2d0fd51cd726fdc82d399006497f35a16106fa71
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-01-16 18:30:55 -0300

    Finished Evolution chain style

--- src/components/EvolutionCard.tsx
+++ src/components/EvolutionCard.tsx
@@ -5,6 +5,7 @@ import { BackgroundTypeColours } from "../types/TypeColoursInterfaces";
 
 interface EvolutionCardProps {
   pokemonEvolution: string;
+  evolutionChainSize: number
 }
 
 export default function EvolutionCard(props: EvolutionCardProps) {
@@ -17,7 +18,6 @@ export default function EvolutionCard(props: EvolutionCardProps) {
 
   useEffect(() => {
     axios.get(props.pokemonEvolution).then(({ data }) => {
-      console.log(data);
       setEvolutionInfos({
         id: data.id,
         name: data.name,
@@ -28,7 +28,7 @@ export default function EvolutionCard(props: EvolutionCardProps) {
   }, []);
 
   return (
-    <Link className="w-1/3 m-4 rounded-full" to={`/pokemon/${evolutionInfos.id}`}>
+    <Link className={`${props.evolutionChainSize > 3 ? "w-1/5" : "w-1/3"} m-3 rounded-full`} to={`/pokemon/${evolutionInfos.id}`}>
       <div
         className={`p-6 rounded-full ${
           BackgroundTypeColours[
@@ -39,7 +39,7 @@ export default function EvolutionCard(props: EvolutionCardProps) {
         <img src={evolutionInfos.sprite} alt={evolutionInfos.name} />
       </div>
       <h1 className="mt-[.2rem] text-center text-xl font-poppins font-semibold tracking-wider">
-        #{evolutionInfos.id}{"\n"}
+        #{evolutionInfos.id} {" "}
         {evolutionInfos.name.replace(/^\w/, (c) => c.toUpperCase())}
       </h1>
     </Link>
--- src/components/Evolutions.tsx
+++ src/components/Evolutions.tsx
@@ -11,18 +11,21 @@ export default function Evolutions() {
     let current = chain;
     const urls = [];
 
-    while (current.evolves_to.length > 0) {   
+    while (current.evolves_to.length > 0) {
       if (current.evolves_to.length === 1) {
         urls.push(`https://pokeapi.co/api/v2/pokemon/${current.species.name}/`);
       } else {
-        current.evolves_to.forEach(evol => urls.push(`https://pokeapi.co/api/v2/pokemon/${evol.species.name}/`))
+        current.evolves_to.forEach((evol) =>
+          urls.push(`https://pokeapi.co/api/v2/pokemon/${evol.species.name}/`)
+        );
       }
 
       current = current.evolves_to[0];
     }
+    if (urls.length < 3)
+      urls.push(`https://pokeapi.co/api/v2/pokemon/${current.species.name}/`);
 
-    urls.push(`https://pokeapi.co/api/v2/pokemon/${current.species.name}/`);
-    setEvolutionsURL(urls); 
+    setEvolutionsURL(urls);
   };
 
   useEffect(() => {
@@ -38,7 +41,11 @@ export default function Evolutions() {
   return (
     <div className="flex flex-wrap justify-center items-center">
       {evolutionsURL.map((url) => (
-        <EvolutionCard pokemonEvolution={url} key={url}/>
+        <EvolutionCard
+          pokemonEvolution={url}
+          evolutionChainSize={evolutionsURL.length}
+          key={url}
+        />
       ))}
     </div>
   );
