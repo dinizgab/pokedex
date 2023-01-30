4c5c490804f65a2fa0c4738d438496b34fdc14ef
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-01-30 14:51:02 -0300

    Refactoring the getEvolutions code

--- src/components/Evolutions.tsx
+++ src/components/Evolutions.tsx
@@ -11,21 +11,15 @@ export default function Evolutions() {
     let current = chain;
     const urls = [];
 
-    while (current.evolves_to.length > 0) {
-      if (current.evolves_to.length === 1) {
-        urls.push(`https://pokeapi.co/api/v2/pokemon/${current.species.name}/`);
-      } else {
-        current.evolves_to.forEach((evol: any) =>
-          urls.push(`https://pokeapi.co/api/v2/pokemon/${evol.species.name}/`)
-        );
-      }
-
+    while (current.evolves_to.length) {
+      urls.push(`https://pokeapi.co/api/v2/pokemon/${current.species.name}/`);
+      current.evolves_to.forEach((evol: any) =>
+        urls.push(`https://pokeapi.co/api/v2/pokemon/${evol.species.name}/`)
+      );
       current = current.evolves_to[0];
     }
-    if (urls.length < 3)
-      urls.push(`https://pokeapi.co/api/v2/pokemon/${current.species.name}/`);
-
-    setEvolutionsURL(urls);
+  
+    setEvolutionsURL([...new Set(urls)]);
   };
 
   useEffect(() => {
